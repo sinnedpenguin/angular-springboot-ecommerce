a1fb1b0119782541455aefceb3bfa96cf8728e59
sinnedpenguin <sinnedpenguin@proton.me>
2023-08-29 02:43:05 +0800

    Complete stripe payment integration for order

--- frontend/src/app/payment/payment.component.ts
+++ frontend/src/app/payment/payment.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { PaymentService } from '../services/payment.service';
+import { loadStripe, Stripe, StripeCardElement } from '@stripe/stripe-js';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-payment',
@@ -6,5 +9,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./payment.component.css']
 })
 export class PaymentComponent {
+  stripe: Stripe | null = null;
+  card: StripeCardElement | null = null;
 
+  constructor(private paymentService: PaymentService, private router: Router) {}
+
+  async ngOnInit() {
+    this.stripe = await loadStripe('pk_test_51NiZKsLtTFoBOb8wFWpjprx5p0Wa26RUc0qGXK5HKZNSUrdPzIDPQSQadSoQkdQ8CgED8arZIAuygiiRmTiWeJ0q00PF5pum1G');
+
+    if (this.stripe) {
+      const elements = this.stripe.elements();
+      this.card = elements.create('card');
+      this.card.mount('#card-element');
+    }
+  }
+
+  async onSubmit() {
+    if (this.stripe && this.card) {
+      const { paymentMethod, error } = await this.stripe.createPaymentMethod({
+        type: 'card',
+        card: this.card,
+      });
+
+      if (error) {
+        console.error(error);
+      } else if (paymentMethod) {
+        const userId = localStorage.getItem('user_id');
+        const access_token = localStorage.getItem('access_token');
+
+        if (userId !== null && access_token !== null) {
+          this.paymentService.checkout(userId, access_token, paymentMethod.id).subscribe(
+            (response) => {
+              alert("Payment successful!");
+              this.router.navigate(['/order']);
+              console.log(response);
+            },
+            (error) => {
+              console.error(error);
+            }
+          );
+        }
+      }
+    }
+  }
 }
